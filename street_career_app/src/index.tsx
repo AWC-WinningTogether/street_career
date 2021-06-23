683250dcc23311cfc7fe6fd74167105490f1217d
Manikanta Challa <manikanta@tinkerix.com>
2021-06-23 10:44:07 +0530

    Sample component done

--- street_career_app/src/index.tsx
+++ street_career_app/src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => (
-    <h1>My React and TypeScript App!</h1>
-);
+import { HomeComponent } from "./components/home";
 
 ReactDOM.render(
-    <App />,
+    <HomeComponent name="Manikanta" />,
     document.getElementById("root")
 );
